ea6152d98ec79a3944e62bf24ce287c1e0fec1f5
UlianaVladko <ulya.vladko@mail.ru>
2025-11-12 16:05:36 +0300

    конструкция switch-case

--- lesson2sequel/main.go
+++ lesson2sequel/main.go
@@ -3,19 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	var a, b int
-	a = 1
-	b = 2
+	var a int
+	a = 3
+	// b = 2
 	
-	incB := func() bool{
-		b += 1 // b =3
-		return true
-	}
-	if (a == 1) /* true */ || incB() /* даж не выполнялось */ {
-		fmt.Println("All good")
-	} else {
-		fmt.Println("Bad value")
+	// if c := 4; c > b && c > a {
+	// 	fmt.Println("c > b and c > a")
+	// }
+
+	switch a {
+	case 1:
+		fmt.Println("1")
+	case 2:
+		fmt.Println("2")
+	case 3, 4:
+		fmt.Println("3 or 4")
+	default:
+		fmt.Println("Default case")
 	}
 
-	fmt.Println(a,b) //предполагаем 1, 3; НО выводит 1, 2
+	// incB := func() bool{
+	// 	b += 1 // b =3
+	// 	return true
+	// }
+	// if (a == 1) /* true */ || incB() /* даж не выполнялось */ {
+	// 	fmt.Println("All good")
+	// } else {
+	// 	fmt.Println("Bad value")
+	// }
+
+	// fmt.Println(a,b) //предполагаем 1, 3; НО выводит 1, 2
 }
