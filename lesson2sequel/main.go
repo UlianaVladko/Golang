a4fc2442e8ef43a6451cde207f1a71dac5b3dc5e
UlianaVladko <ulya.vladko@mail.ru>
2025-11-12 17:11:19 +0300

    циклы и задание на бесконечный цикл с выводом

--- lesson2sequel/main.go
+++ lesson2sequel/main.go
@@ -3,24 +3,84 @@ package main
 import "fmt"
 
 func main() {
-	var a int
-	a = 3
+
+/*
+ЗАДАЧА
+Написать прграмму на ЯП Go с следующим функционалом:
+В бесконечном цикле считывать от пользователя числа.
+И складывать их друг с другом.
+Если пользователь ввел 0 (как число) - вывести на
+экран полученную сумму и выйти из программы.
+*/
+var sum int
+fmt.Println("Начинайте вводить числа")
+	for {
+		var value int
+		fmt.Scan(&value)
+		sum +=value
+		if value == 0{
+			break
+		}
+	}
+fmt.Println("Сумма: ", sum)
+
+	// v := 0
+	// for i := 0; i < 10; i++ {
+	// 	// break - закончить цикл
+	// 	// continue - перейти к следующей иттерации
+	// 	if i%2 != 0 || i == 0 {
+	// 		continue
+	// 	}
+	// 	fmt.Println(i)
+
+		// v++
+		// fmt.Println(v)
+		// if v >= 5 {
+		// 	break
+		// }
+	// }
+
+	// v := 0
+	// i := 0
+	// for i < 10 {
+	// 	v++
+	// 	fmt.Println(v)
+	// 	i++
+	// }
+
+	// бесконечный цикл
+	// v := 0
+	// for {
+	// 	v++
+	// 	fmt.Println(v)
+	// }
+
+	// v := 0
+	// for i := 0; i < 10; i++ {
+	// 	v++
+	// 	fmt.Println(v)
+	// }
+
+	// var a int
+	// a = 3
 	// b = 2
-	
+
 	// if c := 4; c > b && c > a {
 	// 	fmt.Println("c > b and c > a")
 	// }
 
-	switch a {
-	case 1:
-		fmt.Println("1")
-	case 2:
-		fmt.Println("2")
-	case 3, 4:
-		fmt.Println("3 or 4")
-	default:
-		fmt.Println("Default case")
-	}
+	// switch {
+	// case a >= 3:
+	// 	fmt.Println("more than 3")
+	// 	fallthrough
+	// case a >= 2:
+	// 	fmt.Println("more than 2")
+	// 	fallthrough
+	// case a >= 1:
+	// 	fmt.Println("more than 1")
+	// default:
+	// 	fmt.Println("Default case")
+	// }
 
 	// incB := func() bool{
 	// 	b += 1 // b =3
