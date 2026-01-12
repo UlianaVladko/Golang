cd9f4e95113a23c7d3b97199e62715f3e6cb4b19
UlianaVladko <ulya.vladko@mail.ru>
2026-01-12 12:32:15 +0300

    Пуш вебинара 17 от 04.12.2025 simple client and POST

--- lesson14/client.go
+++ lesson14/client.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
 func main() {
-	response, err := http.Get("http://127.0.0.1:8080/")
+	// response, err := http.Get("http://127.0.0.1:8080/")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	client := http.Client{
+		Timeout: 1 * time.Second,
+	}
+	data := []byte(`{"key": "value"}`)
+
+	request, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:8080/", bytes.NewBuffer(data))
+
 	if err != nil {
 		fmt.Println(err)
-		return
 	}
+
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Cookie", "123456789")
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
