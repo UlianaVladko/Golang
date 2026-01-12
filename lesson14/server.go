42c9c0e787d874f98c240843a9e98694d412603b
UlianaVladko <ulya.vladko@mail.ru>
2026-01-12 12:39:01 +0300

    Пуш вебинара 17 от 04.12.2025 GET/POST

--- lesson14/server.go
+++ lesson14/server.go
@@ -5,8 +5,13 @@ import (
 	"net/http"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request)  {
-	w.Write([]byte("Succes request"))
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		w.Write([]byte("Succes POST request"))
+	} else if r.Method == http.MethodGet {
+		w.Write([]byte(`{"status": "Succes GET"}`))
+	}
+
 }
 
 func main() {
